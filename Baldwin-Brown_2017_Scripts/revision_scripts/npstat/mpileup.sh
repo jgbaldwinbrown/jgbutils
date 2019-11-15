b2da1dc63348d95ebf251fa2905c1f8fcec149d5
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2019-11-15 15:39:16 -0700

    fixed npstat

--- Baldwin-Brown_2017_Scripts/revision_scripts/npstat/mpileup.sh
+++ Baldwin-Brown_2017_Scripts/revision_scripts/npstat/mpileup.sh
@@ -5,6 +5,6 @@ set -e
 samtools mpileup \
     -C50 \
     -f data/ref/shrimp_qmerged.quiver.fasta \
-    data/merged-realigned.bam | \
+    data/merged-realigned-deduped2.bam | \
 pigz -p 7 \
 > inter/merged-realigned.mpileup.gz
--- Baldwin-Brown_2017_Scripts/revision_scripts/npstat/run_npstat.sh
+++ Baldwin-Brown_2017_Scripts/revision_scripts/npstat/run_npstat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-./npstat_multi.py <(gunzip -c inter/merged-realigned.mpileup.gz) -n 100 -l 2 > out/npstat_out.txt
+../npstat/npstat_multi.py <(gunzip -c inter/merged-realigned.mpileup.gz) -n 100 -l 2 > out/npstat_out.txt
