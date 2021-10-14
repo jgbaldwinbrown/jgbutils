7c2d9dda6b6e69e8598160db54d52644104eebb3
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-10-14 17:05:12 -0600

    upgraded distortion_finder to work with improved normalization, also added fdr correction

--- Distortion_2019/statistical_test/distortion_finder.R
+++ Distortion_2019/statistical_test/distortion_finder.R
@@ -36,7 +36,6 @@ main <- function() {
         exit("This test does not exist!")
     }
 }
-
 get_args <- function() {
     parser <- ArgumentParser()
     parser$add_argument("input", help="Input file.")
@@ -44,8 +43,10 @@ get_args <- function() {
     parser$add_argument("-O", "--txt_out2", default="out2.txt", help="Output path for means of lm-corrected data.")
     parser$add_argument("-p", "--pdf_out", default="out.pdf", help="Output file for plot of lm-corrected data.")
     parser$add_argument("-P", "--pdf_out2", default="out2.pdf", help="Output file for plot of lm-corrected t-test.")
+    parser$add_argument("-F", "--pdf_out3", default="out3.pdf", help="Output file for plot of lm-corrected t-test (fdr-corrected).")
     parser$add_argument("-m", "--pdf_title", default="Chromosome means of deviation from lm", help="Title for pdf.")
     parser$add_argument("-M", "--pdf_title2", default="T-test of deviation from lm", help="Title for pdf.")
+    parser$add_argument("-N", "--pdf_title3", default="T-test of deviation from lm (FDR-corrected)", help="Title for pdf.")
     parser$add_argument("-t", "--test", default="freq_f", help="Statistical test to perform (options are freq_f, freq_f_ill, coverage_t, coverage_t_cov, coverage_t_ill, coverage_t_cov_ill)")
     parser$add_argument("-r", "--rename", default=FALSE, help="Rename columns to match standard", action="store_true")
     args <- parser$parse_args()
@@ -71,6 +72,8 @@ run_freq_f <- function(args) {
     pdf_title = args$pdf_title
     pdf_out2 = args$pdf_out2
     pdf_title2 = args$pdf_title2
+    pdf_out3 = args$pdf_out3
+    pdf_title3 = args$pdf_title3
     # data <- read.table(input)
     data <- as.data.frame(fread(input))
     if (args$rename) {
@@ -91,10 +94,10 @@ run_freq_f <- function(args) {
     pos_stats$freq_bloodmeans = sapply(levels(factor(data_blood$pos)), function(x){mean(data_blood$freq[data_blood$pos==x], na.rm=TRUE)})
     pos_stats$freq_bloodsd = sapply(levels(factor(data_blood$pos)), function(x){sd(data_blood$freq[data_blood$pos==x], na.rm=TRUE)})
 
-    data$freq_bloodmean = apply(data, 1, function(x){pos_stats$freq_bloodmeans[pos_stats$pos==x["pos"]]})
-    data$freq_bloodsd = apply(data, 1, function(x){pos_stats$freq_bloodsd[pos_stats$pos==x["pos"]]})
+    data$bloodmean = apply(data, 1, function(x){pos_stats$freq_bloodmeans[pos_stats$pos==x["pos"]]})
+    data$bloodsd = apply(data, 1, function(x){pos_stats$freq_bloodsd[pos_stats$pos==x["pos"]]})
 
-    data$freq_nor = (data$freq - data$freq_bloodmean) / data$freq_bloodsd
+    data$freq_nor = (data$freq - data$bloodmean) / data$bloodsd
 
     print(data)
     l = lm(data, formula = freq_nor ~ sample + tissue)
@@ -170,10 +173,10 @@ run_coverage_t <- function(args) {
     pos_stats$freq_bloodmeans = sapply(levels(factor(data_blood$pos)), function(x){mean(data_blood$freq[data_blood$pos==x], na.rm=TRUE)})
     pos_stats$freq_bloodsd = sapply(levels(factor(data_blood$pos)), function(x){sd(data_blood$freq[data_blood$pos==x], na.rm=TRUE)})
     
-    data$freq_bloodmean = apply(data, 1, function(x){pos_stats$freq_bloodmeans[pos_stats$pos==x["pos"]]})
-    data$freq_bloodsd = apply(data, 1, function(x){pos_stats$freq_bloodsd[pos_stats$pos==x["pos"]]})
+    data$bloodmean = apply(data, 1, function(x){pos_stats$freq_bloodmeans[pos_stats$pos==x["pos"]]})
+    data$bloodsd = apply(data, 1, function(x){pos_stats$freq_bloodsd[pos_stats$pos==x["pos"]]})
     
-    data$freq_nor = (data$freq - data$freq_bloodmean) / data$freq_bloodsd
+    data$freq_nor = (data$freq - data$bloodmean) / data$bloodsd
     
     l = lm(data, formula = freq_nor ~ sample + tissue)
     p = predict(l, data)
@@ -223,6 +226,8 @@ run_coverage_t_cov <- function(args) {
     pdf_title = args$pdf_title
     pdf_out2 = args$pdf_out2
     pdf_title2 = args$pdf_title2
+    pdf_out3 = args$pdf_out3
+    pdf_title3 = args$pdf_title3
     
     # data <- read.table(input)
     data <- as.data.frame(fread(input))
@@ -239,44 +244,63 @@ run_coverage_t_cov <- function(args) {
     data$sample = factor(data$sample)
     
     data_blood = data[data$tissue=="blood",]
+
+    print(1)
     
-    pos_stats = data.frame(pos=levels(factor(data_blood$pos)))
-    pos_stats$freq_bloodmeans = sapply(
-        levels(factor(data_blood$pos)), 
-        function(x) {
-            mean(data_blood$freq[data_blood$pos==x], na.rm=TRUE)
-        }
-    )
-    pos_stats$freq_bloodsd = sapply(
-        levels(factor(data_blood$pos)),
-        function(x) {
-            sd(data_blood$freq[data_blood$pos==x], na.rm=TRUE)
-        }
-    )
+    # pos_stats = data.frame(pos=levels(factor(data_blood$pos)))
+    # pos_stats$freq_bloodmeans = sapply(
+    #     levels(factor(data_blood$pos)), 
+    #     function(x) {
+    #         mean(data_blood$freq[data_blood$pos==x], na.rm=TRUE)
+    #     }
+    # )
+    # pos_stats$freq_bloodsd = sapply(
+    #     levels(factor(data_blood$pos)),
+    #     function(x) {
+    #         sd(data_blood$freq[data_blood$pos==x], na.rm=TRUE)
+    #     }
+    # )
+
+    print(2)
     
-    data$freq_bloodmean = apply(data, 1, function(x){pos_stats$freq_bloodmeans[pos_stats$pos==x["pos"]]})
-    data$freq_bloodsd = apply(data, 1, function(x){pos_stats$freq_bloodsd[pos_stats$pos==x["pos"]]})
+    # data$freq_bloodmean = apply(data, 1, function(x){pos_stats$freq_bloodmeans[pos_stats$pos==x["pos"]]})
+    # print(3)
+    # data$freq_bloodsd = apply(data, 1, function(x){pos_stats$freq_bloodsd[pos_stats$pos==x["pos"]]})
+    # print(4)
     
-    data$freq_nor = (data$freq - data$freq_bloodmean) / data$freq_bloodsd
+    data$freq_nor = (data$freq - data$bloodmean) / data$bloodsd
+    print(5)
     
+    data = data[!is.nan(data$freq_nor) & !is.na(data$freq_nor) & !is.infinite(data$freq_nor),]
     l = lm(data, formula = freq_nor ~ per_chrom_gc_index + sample + tissue)
+    print(5)
     p = predict(l, data)
+    print(6)
     
     data$p = p
+    print(7)
     data$diff = data$freq_nor - data$p
+    print(8)
     data$z = scale(data$diff)
+    print(9)
     temp = sapply(data$z, function(x){pnorm(-abs(x), mean=0, sd=1, lower.tail=TRUE)})
+    print(10)
     str(temp)
+    print(11)
     data$p = sapply(data$z, function(x){pnorm(-abs(x), mean=0, sd=1, lower.tail=TRUE)})
+    print(12)
     data$indiv_chrom_tissue = paste(data$indiv, data$chrom, data$tissue, sep="_")
+    print(13)
     
     head(data)
     write.table(data, txt_out)
     
+    print(14)
     datameans = aggregate(data$diff, list(data$indiv, data$chrom, data$tissue), mean)
     colnames(datameans) = c("indiv", "chrom", "tissue", "x")
     datameans$indiv_chrom_tissue = paste(datameans$indiv, datameans$chrom, datameans$tissue, sep="_")
     head(datameans)
+    print(15)
     
     a = sapply(datameans$indiv_chrom_tissue,
         function(temp){
@@ -286,14 +310,24 @@ run_coverage_t_cov <- function(args) {
     )
     datameans$p = a
     
+    print(16)
     write.table(datameans, txt_out2)
     
+    print(17)
     pdf(pdf_out,height=10,width=3)
-    ggplot(data=datameans, aes(chrom, x)) + geom_bar(stat="identity") + facet_grid(indiv~tissue) + ggtitle(pdf_title)
+    print(ggplot(data=datameans, aes(chrom, x)) + geom_bar(stat="identity") + facet_grid(indiv~tissue) + ggtitle(pdf_title))
     dev.off()
     
+    print(18)
     pdf(pdf_out2,height=10,width=3)
-    ggplot(data=datameans, aes(chrom, -log10(p))) + geom_bar(stat="identity") + facet_grid(indiv~tissue) + ggtitle(pdf_title2)
+    print(ggplot(data=datameans, aes(chrom, -log10(p))) + geom_bar(stat="identity") + facet_grid(indiv~tissue) + ggtitle(pdf_title2))
+    dev.off()
+
+    datameans$fdr_p = p.adjust(datameans$p, method = "BH")
+    
+    print(19)
+    pdf(pdf_out3,height=10,width=3)
+    print(ggplot(data=datameans, aes(chrom, -log10(fdr_p))) + geom_bar(stat="identity") + facet_grid(indiv~tissue) + ggtitle(pdf_title3))
     dev.off()
 }
 
