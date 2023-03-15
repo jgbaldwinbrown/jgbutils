9e75b798f331cf852abed21088d1cd70a0cdf320
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2023-03-15 15:14:51 -0600

    normalizer v1 working

---
+++ Distortion_2019/statistical_test/normalizer/go.mod
@@ -0,0 +1,5 @@
+module github.com/jgbaldwinbrown/normalizer
+
+go 1.18
+
+require github.com/montanaflynn/stats v0.7.0
--- Distortion_2019/statistical_test/normalizer/normalizer.go
+++ Distortion_2019/statistical_test/normalizer/normalizer.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"strings"
+	"strconv"
 	"encoding/csv"
 	"compress/gzip"
-	"math"
-	"github.com/montanaflynn/stats"
 	"io"
 	"os"
 	"flag"
@@ -12,8 +12,8 @@ import (
 )
 
 func handle(form string) func(...any) error {
-	return func(args ...any) {
-		return fmt.Errorf(form, args)
+	return func(args ...any) error {
+		return fmt.Errorf(form, args...)
 	}
 }
 
@@ -23,6 +23,10 @@ type NamedValSet struct {
 	Names []string
 	Sums map[string]float64
 	Counts map[string]float64
+}
+
+func (s *NamedValSet) Mean(id string) float64 {
+	return s.Sums[id] / s.Counts[id]
 }
 
 func Open(path string) (*csv.Reader, *gzip.Reader, *os.File, error) {
@@ -50,7 +54,7 @@ func NamedCols(path string, names []string) ([]int, error) {
 	h := handle("NamedCols: %w")
 
 	cr, gr, r, e := Open(path)
-	if e { return nil, h(e) }
+	if e != nil { return nil, h(e) }
 	defer r.Close()
 	defer gr.Close()
 
@@ -69,7 +73,7 @@ func NamedCols(path string, names []string) ([]int, error) {
 	}
 
 	if len(idxs) != len(names) {
-		return nil, h(fmt.Errorf("len(idxs) %v != len(names) %v", len(idxs), len(names))
+		return nil, h(fmt.Errorf("len(idxs) %v != len(names) %v", len(idxs), len(names)))
 	}
 
 	return idxs, nil
@@ -84,7 +88,7 @@ func ValCol(path, valname string) (int, error) {
 	return cols[0], nil
 }
 
-func IdCols(path, idnames []string) ([]int, error) {
+func IdCols(path string, idnames []string) ([]int, error) {
 	return NamedCols(path, idnames)
 }
 
@@ -98,14 +102,14 @@ func NewNamedValSet() *NamedValSet {
 func CalcMeans(path string, valcol int, idnames []string, idcols []int) ([]*NamedValSet, error) {
 	h := handle("CalcMeans: %w")
 
-	cr, gr, fp, e := Open(inpath)
-	if e { return h(e) }
+	cr, gr, fp, e := Open(path)
+	if e != nil { return nil, h(e) }
 	defer fp.Close()
 	defer gr.Close()
 
 	sets := []*NamedValSet{}
 	for i, name := range idnames {
-		set := NewNamedValSet()
+		s := NewNamedValSet()
 		s.ColName = name
 		s.Idx = idcols[i]
 		sets = append(sets, s)
@@ -127,23 +131,117 @@ func CalcMeans(path string, valcol int, idnames []string, idcols []int) ([]*Name
 	return sets, nil
 }
 
+func (s *NamedValSet) Add(val float64, id string) {
+	s.Sums[id] += val
+	s.Counts[id]++
+}
+
+func (s *NamedValSet) AddResid(val float64, line []string, means []*NamedValSet, id string) {
+	resid := val
+	for _, mean := range means {
+		resid -= mean.Mean(line[mean.Idx])
+	}
+	s.Add(resid, id)
+}
+
+func CalcSerialMean(path string, valcol int, means []*NamedValSet, idname string, idcol int) (*NamedValSet, error) {
+	h := handle("CalcSerialMean: %w")
+
+	cr, gr, fp, e := Open(path)
+	if e != nil { return nil, h(e) }
+	defer fp.Close()
+	defer gr.Close()
+
+	s := NewNamedValSet()
+	s.ColName = idname
+	s.Idx = idcol
+
+	for line, e := cr.Read(); e != io.EOF; line, e = cr.Read() {
+		if e != nil { return s, h(e) }
+
+		if len(line) <= valcol { continue }
+		val, e := strconv.ParseFloat(line[valcol], 64)
+		if e != nil { continue }
+
+		if len(line) <= s.Idx { continue }
+		s.AddResid(val, line, means, line[s.Idx])
+	}
+
+	return s, nil
+}
+
+func CalcSerialMeans(path string, valcol int, idnames []string, idcols []int) ([]*NamedValSet, error) {
+	h := handle("CalcSerialMeans: %w")
+
+	var means []*NamedValSet
+	for i, name := range idnames {
+		mean, e := CalcSerialMean(path, valcol, means, name, idcols[i])
+		if e != nil { return nil, h(e) }
+		means = append(means, mean)
+	}
+	return means, nil
+}
+
+func NormOne(line []string, valcol int, means []*NamedValSet) (float64, error) {
+	h := handle("NormOne: %w")
+
+	if len(line) <= valcol { return 0, h(fmt.Errorf("line too short")) }
+	val, e := strconv.ParseFloat(line[valcol], 64)
+	if e != nil { return 0, h(e) }
+
+	resid := val
+
+	for _, mean := range means {
+		if len(line) <= mean.Idx { return 0, h(fmt.Errorf("line too short")) }
+		resid -= mean.Mean(line[mean.Idx])
+	}
+	return resid, nil
+}
+
 func Norm(path string, w io.Writer, valcol int, means []*NamedValSet) error {
-	
+	h := handle("Norm: %w")
+
+	cr, gr, fp, e := Open(path)
+	if e != nil { return h(e) }
+	defer fp.Close()
+	defer gr.Close()
+
+	cw := csv.NewWriter(w)
+	cw.Comma = rune('\t')
+	defer cw.Flush()
+
+	line, e := cr.Read()
+	if e != nil { return h(e) }
+	line = append(line, "norm")
+	e = cw.Write(line)
+	if e != nil { return h(e) }
+
+	for line, e := cr.Read(); e != io.EOF; line, e = cr.Read() {
+		if e != nil { return h(e) }
+
+		norm, e := NormOne(line, valcol, means)
+		if e != nil { continue }
+		line = append(line, fmt.Sprintf("%f", norm))
+		e = cw.Write(line)
+		if e != nil { continue }
+	}
+
+	return nil
 }
 
 func Run(path string, w io.Writer, valcolname string, idcolsnames []string) error {
 	h := handle("Run: %w")
 
-	valcol, e := ValCol(path)
+	valcol, e := ValCol(path, valcolname)
 	if e != nil { return h(e) }
 
-	idcols, e := IdCols(path)
+	idcols, e := IdCols(path, idcolsnames)
 	if e != nil { return h(e) }
 
-	means, e := CalcMeans(path, valcol, idcolsnames, idcols)
+	means, e := CalcSerialMeans(path, valcol, idcolsnames, idcols)
 	if e != nil { return h(e) }
 
-	e := Norm(path, w, valcol, means)
+	e = Norm(path, w, valcol, means)
 	if e != nil { return h(e) }
 
 	return nil
@@ -169,6 +267,6 @@ func main() {
 		panic(fmt.Errorf("could not parse -id %v", *idcolsp))
 	}
 
-	e := Run(*inpp, os.Stdout)
+	e := Run(*inpp, os.Stdout, *valcolp, idcols)
 	if e != nil { panic(e) }
 }
